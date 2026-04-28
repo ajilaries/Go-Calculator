07522c9b2bf85e0e2034e1a2e6922f7f304e64b3
Aju <ajilaries20@gmail.com>
2026-04-28 14:02:26 +0530

    parsing of the variables

--- internal/calculator/parser.go
+++ internal/calculator/parser.go
@@ -10,20 +10,33 @@ import (
 func Parse(input string) (models.Expression, error) {
 	parts := strings.Fields(input)
 
-	if len(parts) != 3 {
-		return models.Expression{}, ErrInvalidOp
+	// unary (sqrt 25)
+	if len(parts) == 2 {
+		val, err := strconv.ParseFloat(parts[1], 64)
+		if err != nil {
+			return models.Expression{}, ErrInvalidOp
+		}
+
+		return models.Expression{
+			Operator: parts[0],
+			Operands: []float64{val},
+		}, nil
 	}
 
-	a, err1 := strconv.ParseFloat(parts[0], 64)
-	b, err2 := strconv.ParseFloat(parts[2], 64)
+	// binary (10 + 5)
+	if len(parts) == 3 {
+		a, err1 := strconv.ParseFloat(parts[0], 64)
+		b, err2 := strconv.ParseFloat(parts[2], 64)
 
-	if err1 != nil || err2 != nil {
-		return models.Expression{}, ErrInvalidOp
+		if err1 != nil || err2 != nil {
+			return models.Expression{}, ErrInvalidOp
+		}
+
+		return models.Expression{
+			Operator: parts[1],
+			Operands: []float64{a, b},
+		}, nil
 	}
 
-	return models.Expression{
-		A:  a,
-		B:  b,
-		Op: parts[1],
-	}, nil
+	return models.Expression{}, ErrInvalidOp
 }
