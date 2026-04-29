c04352874ffebaaedf4190aeaa16bbdb4f309949
Aju <ajilaries20@gmail.com>
2026-04-29 10:18:55 +0530

    parser has updated for proper input and output

--- internal/calculator/parser.go
+++ internal/calculator/parser.go
@@ -1,40 +1,45 @@
 package calculator
 
 import (
+	"regexp"
 	"strconv"
-	"strings"
 
 	"go-calculator/internal/models"
 )
 
 func Parse(input string) (models.Expression, error) {
-	parts := strings.Fields(input)
+	// Match: number operator number
+	re := regexp.MustCompile(`^\s*([\d\.]+)\s*([\+\-\*/\^%])\s*([\d\.]+)\s*$`)
+	match := re.FindStringSubmatch(input)
 
-	// unary (sqrt 25)
-	if len(parts) == 2 {
-		val, err := strconv.ParseFloat(parts[1], 64)
-		if err != nil {
-			return models.Expression{}, ErrInvalidOp
-		}
-
-		return models.Expression{
-			Operator: parts[0],
-			Operands: []float64{val},
-		}, nil
-	}
-
-	// binary (10 + 5)
-	if len(parts) == 3 {
-		a, err1 := strconv.ParseFloat(parts[0], 64)
-		b, err2 := strconv.ParseFloat(parts[2], 64)
+	// binary operation
+	if len(match) == 4 {
+		a, err1 := strconv.ParseFloat(match[1], 64)
+		b, err2 := strconv.ParseFloat(match[3], 64)
 
 		if err1 != nil || err2 != nil {
 			return models.Expression{}, ErrInvalidOp
 		}
 
 		return models.Expression{
-			Operator: parts[1],
+			Operator: match[2],
 			Operands: []float64{a, b},
+		}, nil
+	}
+
+	// unary operation (sqrt 25)
+	re2 := regexp.MustCompile(`^\s*(sqrt|log)\s*([\d\.]+)\s*$`)
+	match2 := re2.FindStringSubmatch(input)
+
+	if len(match2) == 3 {
+		val, err := strconv.ParseFloat(match2[2], 64)
+		if err != nil {
+			return models.Expression{}, ErrInvalidOp
+		}
+
+		return models.Expression{
+			Operator: match2[1],
+			Operands: []float64{val},
 		}, nil
 	}
 
